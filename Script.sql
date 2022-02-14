0a850c10d9946340952d6bc038a093c6c652562c
rbabarov <90253870+rbabarov@users.noreply.github.com>
2022-02-14 14:00:20 +0500

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,19 +1,49 @@
-create table if not exists player (
+create table if not exists genre (
 	id serial primary key,
-	name varchar(40) unique not null,
+	names varchar(40) unique not null	
+);
+
+create table if not exists collections (
+	id serial primary key,
+	names varchar(40) not null,
+	years integer check (years > 0)
+);
+
+create table if not exists artist (
+	id serial primary key,
+	names varchar(40) unique not null,
 	surname varchar(60) unique not null
+);
+
+create table if not exists artist_genre (
+	id_artist integer references artist (id),
+	id_genre integer references genre (id),
 );
 
 create table if not exists albums (
 	id serial primary key,
-	players_id integer references player(id),
 	description text,
-	year integer check (year > 0)		
+	years integer check (year > 0)
+);
+
+create table if not exists artist_albums (
+	id serial primary key,
+	artist_id integer references artist(id),
+	albums_id integer references abums(id)
 );
 
 create table if not exists track (
 	id serial primary key,
-	name text not null, 	
+	names text not null, 	
 	duration numeric,
-	product_id integer references albums(id)
+	product_id integer references albums(id),
+	collection_id integer references track_collections(id)
 );
+
+create table if not exists track_collections (
+	id serial primary key,	
+	track_id integer references track(id),
+	collection_id integer references collections(id)
+	
+);
+
